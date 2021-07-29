d77a43a4977c6edb2fd3363b8f5c3e6119ab0d5e
pitiponghms <84966586+pitiponghms@users.noreply.github.com>
2021-07-29 18:12:58 +0700

    AssessmentFrom V1.0

--- Controller/AssessmentFormController.cs
+++ Controller/AssessmentFormController.cs
@@ -22,6 +22,8 @@ namespace TechLineCaseAPI.Controller
                 List<AssessmentMasterModel> models = new List<AssessmentMasterModel>();
                 var list = entity.assessmentform_master
                     .Where(o => o.assessmentform_type.Contains(type))
+                    .OrderBy(o => o.groupno)
+                    .ThenBy(o => o.seqno)
                     .ToList();
 
                 foreach (var item in list)
@@ -56,69 +58,71 @@ namespace TechLineCaseAPI.Controller
             }
         }
 
-        //[HttpGet]
-        //[Route("api/assessment/form/get/{rocaseid}/{type}")]
-        //public IHttpActionResult GetFormView(int rocaseid, string type)
-        //{
-        //    using (mmthapiEntities entity = new mmthapiEntities())
-        //    {
-        //        List<AssessmentFormModel> models = new List<AssessmentFormModel>();
-        //        var list = entity.vAssessmentForms
-        //            .Where(o => o.assessmentform_type.Contains(type))                    
-        //            .Where(o => o.groupno != null)
-        //            .Where(o => o.ro_caseid == rocaseid || (o.ro_caseid == null && o.seqno == 0)) //Get Header
-        //            .ToList();
+        [HttpGet]
+        [Route("api/assessment/form/get/{rocaseid}/{type}")]
+        public IHttpActionResult GetFormView(int rocaseid, string type)
+        {
+            using (mmthapiEntities entity = new mmthapiEntities())
+            {
+                List<AssessmentFormModel> models = new List<AssessmentFormModel>();
+                var list = entity.vAssessmentForms
+                    .Where(o => o.assessmentform_type.Contains(type))
+                    .Where(o => o.ro_caseid == rocaseid || (o.ro_caseid == null && o.seqno == 0)) //Get Header
+                    .OrderBy(o => o.groupno)
+                    .ThenBy(o => o.seqno)
+                    .ToList();
 
-        //        foreach (var item in list)
-        //        {
-        //            AssessmentFormModel model = new AssessmentFormModel()
-        //            {
-        //                Id = item.id,
-        //                Type = item.assessmentform_type,
-        //                GroupNo = item.groupno,
-        //                SeqNo = item.seqno,
-        //                Subject = item.subject,
+                foreach (var item in list)
+                {
+                    AssessmentFormModel model = new AssessmentFormModel()
+                    {
+                        Id = item.id,
+                        Type = item.assessmentform_type,
+                        GroupNo = item.groupno,
+                        SeqNo = item.seqno,
+                        Subject = item.subject,
 
-        //                ChoiceName1 = item.choicename1,
-        //                ChoiceValue1 = item.choicevalue1,
-        //                IsRadio1 = item.isradio1,
-        //                IsOther1 = item.isother1,
+                        ChoiceName1 = item.choicename1,
+                        ChoiceValue1 = item.choicevalue1,
+                        IsRadio1 = item.isradio1,
+                        IsOther1 = item.isother1,
 
-        //                ChoiceName2 = item.choicename2,
-        //                ChoiceValue2 = item.choicevalue2,
-        //                IsRadio2 = item.isradio2,
-        //                IsOther2 = item.isother2,
+                        ChoiceName2 = item.choicename2,
+                        ChoiceValue2 = item.choicevalue2,
+                        IsRadio2 = item.isradio2,
+                        IsOther2 = item.isother2,
 
-        //                ChoiceName3 = item.choicename3,
-        //                ChoiceValue3 = item.choicevalue3,
-        //                IsRadio3 = item.isradio3,
-        //                IsOther3 = item.isother3,
+                        ChoiceName3 = item.choicename3,
+                        ChoiceValue3 = item.choicevalue3,
+                        IsRadio3 = item.isradio3,
+                        IsOther3 = item.isother3,
 
-        //                ChoiceValue = item.choicevalue,
-        //                Comment = item.comment,
-        //            };
+                        ChoiceValue = item.choicevalue,
+                        Message = item.message,
+                        IsComment = item.iscomment,
+                    };
 
-        //            models.Add(model);
-        //        }
-        //        return Json(models);
-        //    }
-        //}
+                    models.Add(model);
+                }
+                return Json(models);
+            }
+        }
 
-        //[HttpGet]
-        //[Route("api/assessment/form/count/{rocaseid}/{type}")]
-        //public IHttpActionResult CountFormView(int rocaseid, string type)
-        //{
-        //    using (mmthapiEntities entity = new mmthapiEntities())
-        //    {
-        //        var list = entity.vAssessmentForms
-        //            .Where(o => o.assessmentform_type.Contains(type))
-        //            .Where(o => o.groupno != null)                    
-        //            .Where(o => o.ro_caseid == rocaseid)
-        //            .ToList();
+        [HttpGet]
+        [Route("api/assessment/form/count/{rocaseid}/{type}")]
+        public IHttpActionResult CountFormView(int rocaseid, string type)
+        {
+            using (mmthapiEntities entity = new mmthapiEntities())
+            {
+                var list = entity.vAssessmentForms
+                    .Where(o => o.assessmentform_type.Contains(type))
+                    .Where(o => o.groupno != null)
+                    .Where(o => o.ro_caseid == rocaseid)
+                    .ToList();
 
-        //        return Json(list.Count());
-        //    }
-        //}
+                return Json(list.Count());
+            }
+        }
 
         [HttpPost]
         [Route("api/assessment/form/post/create")]
@@ -132,9 +136,9 @@ namespace TechLineCaseAPI.Controller
                 AssessmentFormModel model = js.Deserialize<AssessmentFormModel>(json);
                 int? myid;
 
-                if (model.ROCaseId == 0) return new ResultMessage() { Status = "E", Message = "Require RO Case Id" };
+                if (model.ROCaseId == null) return new ResultMessage() { Status = "E", Message = "Require RO Case Id" };
                 if (model.Type == null) return new ResultMessage() { Status = "E", Message = "Require Type" };
-                if (model.GroupNo == null) return new ResultMessage() { Status = "E", Message = "Require Group No." };
+                if (model.GroupNo == null && model.SeqNo == null) return new ResultMessage() { Status = "E", Message = "Require Group No." };
                 if (model.SeqNo == null) return new ResultMessage() { Status = "E", Message = "Require Seq No." };
                 if (model.CreatedBy == null) return new ResultMessage() { Status = "E", Message = "Require Created By" };
 
@@ -174,9 +178,9 @@ namespace TechLineCaseAPI.Controller
             {
                 int? myid;
 
-                if (model.ROCaseId == 0) return null;
+                if (model.ROCaseId == null) return null;
                 if (model.Type == null) return null;
-                if (model.GroupNo == null) return null;
+                if (model.GroupNo == null && model.SeqNo == null) return null;
                 if (model.SeqNo == null) return null;
                 if (model.CreatedBy == null) return null;
 
@@ -190,7 +194,8 @@ namespace TechLineCaseAPI.Controller
                         groupno = model.GroupNo,
                         seqno = model.SeqNo,
                         choicevalue = model.ChoiceValue,
-                        comment = model.Comment,
+                        message = model.Message,
+                        iscomment = model.IsComment,
 
                         CREATED_BY = model.CreatedBy,
                         CREATED_ON = DateTime.Now,
